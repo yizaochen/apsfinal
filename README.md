7b4be261237378d9a1889eb6803899e07dbf6bca
yizaochen <yizaochen@gmail.com>
2021-05-05 16:19:58 +0800

    Initailized

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # apsfinal
+Environment for final project
+
+# Create a virtual environment
+`conda create --name apsfinal python=3.8`
+
+# Activate virtual environment
+`conda activate apsfinal`
+
+# Deactivate
+`conda deactivate`
+
+# Install bentdna package
+`pip install -e .`
+
+# Upgrade package
+`pip install -e . --upgrade`
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(name='apsfinal', 
+      version='0.1',
+      packages=find_packages(),
+      url='https://github.com/yizaochen/apsfinal.git',
+      author='Yizao Chen',
+      author_email='yizaochen@gmail.com',
+      license='MIT',
+      install_requires=[
+          'pandas',
+          'numpy',
+          'matplotlib'
+      ]
+      )
