ee04dce3b4bb6e8bd0afc262a29ec2c48f09a127
yizaochen <yizaochen@gmail.com>
2021-05-30 20:56:36 +0800

    Update

---
+++ ourfinalreport/_build/jupyter_execute/regression_categorical.r
@@ -0,0 +1,27 @@
+strokedata <- read.csv(file = '../data/healthcare-dataset-stroke-data-cleanbmi.csv')
+head(strokedata)
+
+nrow(strokedata)
+
+model_hypert = glm(stroke ~ hypertension, data=strokedata, family=binomial(link="logit"))
+summary(model_hypert)
+
+model_heartd = glm(stroke ~ heart_disease, data=strokedata, family=binomial(link="logit"))
+summary(model_heartd)
+
+model_marry = glm(stroke ~ ever_married, data=strokedata, family=binomial(link="logit"))
+summary(model_marry)
+
+model_worktype = glm(stroke ~ work_type, data=strokedata, family=binomial(link="logit"))
+summary(model_worktype)
+
+model_smoke = glm(stroke ~ smoking_status, data=strokedata, family=binomial(link="logit"))
+summary(model_smoke)
+
+model_full = glm(stroke ~ hypertension + heart_disease + ever_married + smoking_status, data=strokedata, family=binomial(link="logit"))
+summary(model_full)
+
+model_reduce = glm(stroke ~ hypertension + heart_disease + ever_married, data=strokedata, family=binomial(link="logit"))
+summary(model_reduce)
+
+anova(model_reduce, model_full, test="LRT")
---
+++ ourfinalreport/_build/jupyter_execute/regression_numerical.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # 迴歸分析: 數值型變數
+
+# In[ ]:
+
+
+
+
